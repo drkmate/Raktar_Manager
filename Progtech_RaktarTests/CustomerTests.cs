9bf99b56beaae53d4f202cdc23dc602226fa5859
drkmate <61450030+drkmate@users.noreply.github.com>
2021-06-24 08:23:49 +0200

    Add files via upload

---
+++ Progtech_RaktarTests/CustomerTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PRogtech_Raktar;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRogtech_Raktar.Tests
+{
+    [TestClass()]
+    public class CustomerTests
+    {
+        Goods g1 = new Goods("kenyér", 300, "valamik", 1);
+        Goods g2 = new Goods("kenyér2", 350, "valamik", 1);
+        Goods g3 = new Goods("kenyér3", 4000, "valamik", 1);
+        List<Goods> goodsList = new List<Goods>();
+        List<Warehouse> warehouses = new List<Warehouse>();
+        
+        [TestMethod()]
+        public void DeleteFromCartTest()
+        {
+            Customer customer1 = new Customer(2000);
+            customer1.AddToCart(g1);
+            customer1.AddToCart(g2);
+
+            customer1.DeleteFromCart(g2);
+        }
+
+        [TestMethod()]
+        public void BuyTest()
+        {
+            
+            Customer customer1 = new Customer(650);
+            goodsList.Add(g1);
+            goodsList.Add(g2);
+            goodsList.Add(g3);
+            Warehouse wh1 = new Warehouse(2000, goodsList);
+            warehouses.Add(wh1);
+            customer1.AddToCart(g1);
+            customer1.AddToCart(g2);
+            Console.WriteLine(g1.Price);
+            Console.WriteLine( g2.Price);
+            Console.WriteLine(customer1.Cash);
+            customer1.Buy(warehouses);
+        }
+    }
+}
---
+++ Progtech_RaktarTests/WarehouseTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PRogtech_Raktar;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRogtech_Raktar.Tests
+{
+    [TestClass()]
+    public class WarehouseTests
+    {
+        Goods g1 = new Goods("kenyér", 300, "valamik", 1);
+        Goods g2 = new Goods("kenyér", 300, "valamik", 1);
+        List<Goods> goodsList = new List<Goods>();
+        List<Warehouse> warehouses = new List<Warehouse>();
+        List<Goods> cargo = new List<Goods>();
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Warehouse wh1 = new Warehouse(1, goodsList);
+            wh1.Add(g1);
+            wh1.Add(g2);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Warehouse wh1 = new Warehouse(1, goodsList);
+            wh1.Add(g1);
+            wh1.Delete(g1);
+        }
+
+        [TestMethod()]
+        public void SoldTest()
+        {
+            Warehouse wh1 = new Warehouse(1, goodsList);
+            wh1.Add(g1);
+            wh1.Sold(g1);
+        }
+
+        [TestMethod()]
+        public void SupplyTest()
+        {
+            Warehouse wh1 = new Warehouse(100, goodsList);
+            cargo.Add(g1);
+            cargo.Add(g2);
+            wh1.Supply(cargo);
+        }
+    }
+}
